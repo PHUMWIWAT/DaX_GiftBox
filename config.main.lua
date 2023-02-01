93e3a1e49f1ff77999922f194adf742260fcab5e
THEOHM <95518750+PHUMWIWAT@users.noreply.github.com>
2023-02-01 22:54:15 +0700

    Add files via upload

---
+++ config.main.lua
@@ -0,0 +1,42 @@
+DAXTON = DAXTON or {} local sec = 1000 local min = 10000
+
+ESX = exports["es_extended"]:getSharedObject()
+
+DAXTON = {
+
+    ["SetBox"] = {      --# เพิ่มได้ไม่จำกัด
+
+        -- ["ชื่อไอเท่ม"] = ({
+        --     Add = ({
+        --         ({ Type = ("item"), Name = ("a_gold"), Count = ({ 1,2 }), Percent = (100) }),
+        --         ({ Type = ("weapon"), Name = ("WEAPON_BOTTLE"), Count = ({ 1,1 }), Percent = (100) }),
+        --         ({ Type = ("money"), Name = ("money"), Count = ({ 1000,2500 }), Percent = (100) }),
+        --         ({ Type = ("vehicle"), Name = ("furia"), Plate = ("GTA 555"), Percent = (100) }),
+        --     }),
+        --     RemoveItem = true;   --# เปิด/ปิด การลบไอเท่ม
+        -- }),
+
+        ["a_diamond"] = ({
+            Add = ({
+                ({ Type = ("item"), Name = ("a_gold"), Count = ({ 1,2 }), Percent = (100) }),
+                ({ Type = ("weapon"), Name = ("WEAPON_BOTTLE"), Count = ({ 1,1 }), Percent = (100) }),
+                ({ Type = ("money"), Name = ("money"), Count = ({ 1000,2500 }), Percent = (100) }),
+                ({ Type = ("vehicle"), Name = ("furia"), Plate = ("GTA 555"), Percent = (100) }),
+            }),
+            RemoveItem = true;
+        }),
+
+    },
+
+    -- Function
+    ["UPDATE_VEHICLE"] = function(data) --# เอาไว้สำหรับอัปเดทค่าต่างๆ
+
+        -- exports.nc_inventory:AddItem(data.player, {
+        --     name = data.plate:upper(),
+        --     type = 'vehicle_key'
+        -- })
+        -- exports['Bu_Garage']:ADDPLATE(data.player, data.plate:upper())
+    
+        return false,false;
+    end;
+};
---
+++ fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version 'cerulean'
+game 'gta5'
+
+server_script {'source/source-sv.lua','@oxmysql/lib/MySQL.lua',}
+shared_script 'config.main.lua'
