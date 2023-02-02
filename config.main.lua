eaa6a72d3c1feddda944e39a27cf9bc717a79c66
THEOHM <95518750+PHUMWIWAT@users.noreply.github.com>
2023-02-02 18:48:03 +0700

    Update config.main.lua

--- config.main.lua
+++ config.main.lua
@@ -11,7 +11,8 @@ DAXTON = {
         --         ({ Type = ("item"), Name = ("a_gold"), Count = ({ 1,2 }), Percent = (100) }),
         --         ({ Type = ("weapon"), Name = ("WEAPON_BOTTLE"), Count = ({ 1,1 }), Percent = (100) }),
         --         ({ Type = ("money"), Name = ("money"), Count = ({ 1000,2500 }), Percent = (100) }),
-        --         ({ Type = ("vehicle"), Name = ("furia"), Plate = ("GTA 555"), Percent = (100) }),
+        --         ({ Type = ("vehicle"), Name = ("furia"), Plate = ("GTA 555"), Percent = (100) }),  --# change plate
+        --         ({ Type = ("vehicle"), Name = ("thrax"), Percent = (100) }),    --# random plate
         --     }),
         --     RemoveItem = true;   --# เปิด/ปิด การลบไอเท่ม
         -- }),
@@ -21,7 +22,8 @@ DAXTON = {
                 ({ Type = ("item"), Name = ("a_gold"), Count = ({ 1,2 }), Percent = (100) }),
                 ({ Type = ("weapon"), Name = ("WEAPON_BOTTLE"), Count = ({ 1,1 }), Percent = (100) }),
                 ({ Type = ("money"), Name = ("money"), Count = ({ 1000,2500 }), Percent = (100) }),
-                ({ Type = ("vehicle"), Name = ("furia"), Plate = ("GTA 555"), Percent = (100) }),
+                ({ Type = ("vehicle"), Name = ("furia"), Plate = ("GTA 555"), Percent = (100) }),  --# change plate
+                ({ Type = ("vehicle"), Name = ("thrax"), Percent = (100) }),    --# random plate
             }),
             RemoveItem = true;
         }),
